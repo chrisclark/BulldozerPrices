61fbbc636a6fa85a88f40c1af022ee44f1dd63a1
chrisclark <chris@untrod.com>
2013-01-30 09:06:09 -0800

    optimizations, parsing lengths

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 import util
@@ -10,32 +11,43 @@ def get_date_dataframe(date_column):
         "SaleDay": [d.day for d in date_column]
         }, index=date_column.index)
 
+def get_length_dataframe():
+    newCol = []
+    length_column = train["Stick_Length"]
+    for x in length_column:
+        if type(x) == str and len(x.split(' ')) == 2:
+            parts = x.replace('"',"").replace("'","").split(' ')
+            parts = map(float, parts)
+            newCol.append(parts[0] + (parts[1]/12.0))
+        else:
+            newCol.append(np.NaN)
+    return pd.DataFrame({"Stick_Length": newCol}, index=train["Stick_Length"].index)
+
 train, test = util.get_train_test_df()
 
 columns = set(train.columns)
 columns.remove("SalesID")
 columns.remove("SalePrice")
 columns.remove("saledate")
+columns.remove("Stick_Length")
 
-train_fea = get_date_dataframe(train["saledate"])
+train_fea = get_length_dataframe()
+train_fea = train_fea.join(get_date_dataframe(train["saledate"]))
+
 test_fea = get_date_dataframe(test["saledate"])
+test_fea = test_fea.join(get_length_dataframe())
 
 for col in columns:
-    types = set(type(x) for x in train[col])
-    if str in types:
-        s = set(x for x in train[col])
-        str_to_categorical = defaultdict(lambda: -1, [(x[1], x[0]) for x in enumerate(s)])
-        train_fea = train_fea.join(pd.DataFrame({col: [str_to_categorical[x] for x in train[col]]}, index=train.index))
-        test_fea = test_fea.join(pd.DataFrame({col: [str_to_categorical[x] for x in test[col]]}, index=test.index))
+    if train[col].dtype == np.dtype('object'):
+        s = np.unique(train[col].values)
+        mapping = pd.Series([x[0] for x in enumerate(s)], index = s)
+        train_fea = train_fea.join(train[col].map(mapping))
+        test_fea = test_fea.join(test[col].map(mapping))
     else:
         train_fea = train_fea.join(train[col])
         test_fea = test_fea.join(test[col])
 
-rf = RandomForestRegressor(n_estimators=50, n_jobs=1, compute_importances = True)
+rf = RandomForestRegressor(n_estimators=50, n_jobs=1, min_samples_leaf=5)
 rf.fit(train_fea, train["SalePrice"])
 predictions = rf.predict(test_fea)
-imp = sorted(zip(train_fea.columns, rf.feature_importances_), key=lambda tup: tup[1], reverse=True)
-for fea in imp:
-    print(fea)
-
 util.write_submission("random_forest_benchmark.csv", predictions)
