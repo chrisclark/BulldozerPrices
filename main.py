eddd10281c625e20a66f0ca20457d144af9a9263
chrisclark <chris@untrod.com>
2013-02-07 23:20:35 -0800

    refactor, extra data. Something's wrong though

--- main.py
+++ main.py
@@ -1,102 +1,10 @@
-import numpy as np
-import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 import util
-import re
 
-def get_date_dataframe(date_column):
-    return pd.DataFrame({
-        "SaleYear": [d.year for d in date_column],
-        "SaleMonth": [d.month for d in date_column],
-        "SaleDay": [d.day for d in date_column]
-        }, index=date_column.index)
-
-def Stick_Length(target_col):
-    newCol = []
-    for x in target_col:
-        if type(x) == str and len(x.split(' ')) == 2:
-            parts = x.replace('"',"").replace("'","").split(' ')
-            parts = map(float, parts)
-            newCol.append(parts[0] + (parts[1]/12.0))
-        else:
-            newCol.append(np.NaN)
-    return pd.DataFrame({"Stick_Length": newCol}, index=target_col.index)
-
-def Tire_Size(target_col):
-    newCol = []
-    for x in target_col:
-        if type(x) == str:
-            matchObj = re.match(r'[0-9]+(\.[0-9][0-9]?)?', x)
-            if matchObj: newCol.append(float(matchObj.group()))
-            else: newCol.append(np.NaN)
-        else:
-            newCol.append(np.NaN)
-    return pd.DataFrame({"Tire_Size": newCol}, index=target_col.index)
-
-def Undercarriage_Pad_Width(target_col):
-    newCol = []
-    for x in target_col:
-        if type(x) == str:
-            matchObj = re.match(r'[0-9]+(\.[0-9][0-9]?)?', x)
-            if matchObj: newCol.append(float(matchObj.group()))
-            else: newCol.append(np.NaN)
-        else:
-            newCol.append(np.NaN)
-    return pd.DataFrame({"Undercarriage_Pad_Width": newCol}, index=target_col.index)
-
-def YearMade(target_col):
-    return pd.DataFrame({
-        "YearMade": target_col.map(lambda x: 1993.75 if x == 1000 else x),
-        "YearMade_is_1000": target_col.map(lambda x: 1 if x == 1000 else 0)
-    }, index=target_col.index)
-    
-def MachineHoursCurrentMeter(target_col):
-    return pd.DataFrame({
-        "MachineHoursCurrentMeter_is_nan": target_col.map(lambda x: 1 if x == 0 or np.isnan(x) else 0),
-        "MachineHoursCurrentMeter": target_col.map(lambda x: 3946 if x == 0 or np.isnan(x) else x)
-    }, index=target_col.index)
-
-train, test = util.get_train_test_df()
-
-columns = set(train.columns)
-columns.remove("SalesID")
-columns.remove("SalePrice")
-columns.remove("saledate")
-columns.remove("Stick_Length")
-columns.remove("Tire_Size")
-columns.remove("Undercarriage_Pad_Width")
-columns.remove("YearMade")
-columns.remove("MachineHoursCurrentMeter")
-
-train_fea = get_date_dataframe(train["saledate"])
-train_fea = train_fea.join(Stick_Length(train["Stick_Length"]))
-train_fea = train_fea.join(Tire_Size(train["Tire_Size"]))
-train_fea = train_fea.join(Undercarriage_Pad_Width(train["Undercarriage_Pad_Width"]))
-train_fea = train_fea.join(YearMade(train["YearMade"]))
-train_fea = train_fea.join(MachineHoursCurrentMeter(train["MachineHoursCurrentMeter"]))
-
-test_fea = get_date_dataframe(test["saledate"])
-test_fea = test_fea.join(Stick_Length(test["Stick_Length"]))
-test_fea = test_fea.join(Tire_Size(test["Tire_Size"]))
-test_fea = test_fea.join(Undercarriage_Pad_Width(test["Undercarriage_Pad_Width"]))
-test_fea = test_fea.join(YearMade(test["YearMade"]))
-test_fea = test_fea.join(MachineHoursCurrentMeter(test["MachineHoursCurrentMeter"]))
-
-for col in columns:
-    if train[col].dtype == np.dtype('object'):
-        s = np.unique(train[col].values)
-        mapping = pd.Series([x[0] for x in enumerate(s)], index = s)
-        train_fea = train_fea.join(train[col].map(mapping))
-        test_fea = test_fea.join(test[col].map(mapping))
-    else:
-        train_fea = train_fea.join(train[col])
-        test_fea = test_fea.join(test[col])
-
-train_fea.to_csv("fm_train.csv")
-test_fea.to_csv("fm_test.csv")
+train_fea, test_fea = util.getTrainTestFeatureMatrix(build=False)
 
 rf = RandomForestRegressor(n_estimators=50, n_jobs=1, min_samples_leaf=5)
-rf.fit(train_fea, train["SalePrice"])
+rf.fit(train_fea.drop("SalePrice", axis=1), train_fea["SalePrice"])
 predictions = rf.predict(test_fea)
 util.write_submission("random_forest_benchmark.csv", predictions)
 print "done."
--- util.py
+++ util.py
@@ -1,24 +1,151 @@
 from dateutil.parser import parse
 import pandas as pd
 import os
+import re
+import numpy as np
 
 data_path = "C:/Users/Chris/Dropbox/Documents/kaggle/bulldozers/BulldozerPrices/data/"    
 submission_path = "C:/Users/Chris/Dropbox/Documents/kaggle/bulldozers/BulldozerPrices/data/submissions/"
 
-def get_train_df():
-    train = pd.read_csv(data_path + "Train.csv", converters={"saledate": parse})
-    return train 
+FILE_TRAIN = "Train.csv"
+FILE_TEST = "Valid.csv"
+FILE_JOINED_TRAIN = "joined_train.csv"
+FILE_JOINED_TEST = "joined_test.csv"
+FILE_MACHINE_INFO = "Machine_appendix.csv"
 
-def get_test_df():
-    test = pd.read_csv(os.path.join(data_path, "Valid.csv"),
-        converters={"saledate": parse})
-    return test 
+train = None
+test = None
 
-def get_train_test_df():
-    return get_train_df(), get_test_df()
+def get_df(filename, converters=None):
+    return pd.read_csv(os.path.join(data_path, filename), converters=converters)
 
 def write_submission(submission_name, predictions):
-    test = get_test_df()    
-    test = test.join(pd.DataFrame({"SalePrice": predictions}))
+    test_sub = get_df(FILE_TEST)
+    test_sub = test_sub.join(pd.DataFrame({"SalePrice": predictions}))
+    test_sub[["SalesID", "SalePrice"]].to_csv(submission_path + submission_name, index=False)
+    
+def saledate(date_column):
+    return pd.DataFrame({
+        "SaleYear": [d.year for d in date_column],
+        "SaleMonth": [d.month for d in date_column],
+        "SaleDay": [d.day for d in date_column]
+        }, index=date_column.index)
 
-    test[["SalesID", "SalePrice"]].to_csv(submission_path + submission_name, index=False)
+def Stick_Length(target_col):
+    newCol = []
+    for x in target_col:
+        if type(x) == str and len(x.split(' ')) == 2:
+            parts = x.replace('"',"").replace("'","").split(' ')
+            parts = map(float, parts)
+            newCol.append(parts[0] + (parts[1]/12.0))
+        else:
+            newCol.append(np.NaN)
+    return pd.DataFrame({"Stick_Length_IN": newCol}, index=target_col.index)
+
+def Tire_Size(target_col):
+    newCol = []
+    for x in target_col:
+        if type(x) == str:
+            matchObj = re.match(r'[0-9]+(\.[0-9][0-9]?)?', x)
+            if matchObj: newCol.append(float(matchObj.group()))
+            else: newCol.append(np.NaN)
+        else:
+            newCol.append(np.NaN)
+    return pd.DataFrame({"Tire_Size_IN": newCol}, index=target_col.index)
+
+def Undercarriage_Pad_Width(target_col):
+    newCol = []
+    for x in target_col:
+        if type(x) == str:
+            matchObj = re.match(r'[0-9]+(\.[0-9][0-9]?)?', x)
+            if matchObj: newCol.append(float(matchObj.group()))
+            else: newCol.append(np.NaN)
+        else:
+            newCol.append(np.NaN)
+    return pd.DataFrame({"Undercarriage_Pad_Width_IN": newCol}, index=target_col.index)
+
+def YearMade(target_col):
+    return pd.DataFrame({
+        "YearMade_filled": target_col.map(lambda x: 1993.75 if x == 1000 else x),
+        "YearMade_is_1000": target_col.map(lambda x: 1 if x == 1000 else 0)
+    }, index=target_col.index)
+    
+def MachineHoursCurrentMeter(target_col):
+    return pd.DataFrame({
+        "MachineHoursCurrentMeter_is_nan": target_col.map(lambda x: 1 if x == 0 or np.isnan(x) else 0),
+        "MachineHoursCurrentMeter_Filled": target_col.map(lambda x: 3946 if x == 0 or np.isnan(x) else x)
+    }, index=target_col.index)
+
+def BuildCategorical(target_col):
+    s = np.unique(target_col.values)
+    mapping = pd.Series([x[0] for x in enumerate(s)], index = s)
+    return pd.DataFrame({target_col.name + "_fea": target_col.map(mapping)})
+
+def DropFromBoth(cols):
+    global train
+    global test
+    train = train.drop(cols, axis=1)
+    test = test.drop(cols, axis=1)
+
+def AddFeature(featureFn, colName):
+    global train
+    global test
+    train = train.join(featureFn(train[colName])).drop(colName, axis=1)
+    test = test.join(featureFn(test[colName])).drop(colName, axis=1)
+
+def getTrainTestFeatureMatrix(build=True):
+    if build:
+        
+        #read in the raw data
+        global train
+        global test
+        train = get_df(FILE_TRAIN, {"saledate": parse})
+        test = get_df(FILE_TEST, {"saledate": parse})
+        machine_info = get_df(FILE_MACHINE_INFO)
+        
+        #preemptively drop overlapping columns and merge
+        DropFromBoth(['YearMade','fiModelDesc','fiBaseModel','fiSecondaryDesc','fiModelSeries','fiModelDescriptor','fiProductClassDesc','ProductGroup','ProductGroupDesc'])
+        
+        train = train.merge(machine_info, on="MachineID")
+        test = test.merge(machine_info, on="MachineID")
+        
+        #drop the final overlapping column now that we've joined on it
+        DropFromBoth('ModelID_x')
+        
+        #drop salesID because there is no information encoded in it
+        DropFromBoth('SalesID')
+        
+        print "Joining done...writing intermediate file..."
+        train.to_csv("joined_train.csv")
+        test.to_csv("joined_test.csv")
+        print "done."
+        
+        #build a list of all other features we want to construct
+        columns = set(test.columns)
+        
+        #build special features
+        AddFeature(YearMade, "MfgYear")
+        AddFeature(saledate, "saledate")
+        AddFeature(Stick_Length, "Stick_Length")
+        AddFeature(Tire_Size, "Tire_Size")
+        AddFeature(Undercarriage_Pad_Width, "Undercarriage_Pad_Width")
+        AddFeature(MachineHoursCurrentMeter, "MachineHoursCurrentMeter")
+        
+        print "Feature phase 1 complete...writing intermediate file..."
+        train.to_csv("f1_train.csv")
+        test.to_csv("f1_test.csv")
+        print "done."
+        
+        #default feature construction
+        for col in columns:
+            if train[col].dtype == np.dtype('object'):
+                AddFeature(BuildCategorical, col)
+        
+        print "Completed building feature matrix. Writing results to disk."
+        train.to_csv("fm_train.csv")
+        test.to_csv("fm_test.csv")
+        print "done."
+    else:
+        train = pd.read_csv("fm_train.csv")
+        test = pd.read_csv("fm_test.csv")
+    return train, test
